493f24c85e7a6c0025b39699a807e5a7bed64315
salgadotomas89 <salgadotomas@outlook.com>
2023-07-26 10:36:12 -0400

    capture errors contactform

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import BadHeaderError, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
@@ -13,22 +13,29 @@ def home(request):
 
 @csrf_exempt
 def contacto(request):
-    print('hola')
     if request.method == 'POST':
-        print('entre')
         name = request.POST.get('name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         text = request.POST.get('text')
 
-        # Envía el correo electrónico
-        subject = 'Formulario de contacto'
-        message = f'Nombre: {name}\nApellido: {last_name}\nEmail: {email}\nMensaje: {text}'
-        sender_email = settings.EMAIL_HOST_USER
-        recipient_list = ['salgadotomas@icloud.com']  # Coloca aquí la dirección de correo a la que quieres enviar el formulario
-        send_mail(subject, message, sender_email, recipient_list, fail_silently=False)
-        print('hello')
-        return JsonResponse({'message': 'Mensaje enviado exitosamente.'})
+        try:
+            # Envía el correo electrónico
+            subject = 'Formulario de contacto'
+            message = f'Nombre: {name}\nApellido: {last_name}\nEmail: {email}\nMensaje: {text}'
+            sender_email = settings.EMAIL_HOST_USER
+            recipient_list = ['salgadotomas@icloud.com']  # Coloca aquí la dirección de correo a la que quieres enviar el formulario
+            send_mail(subject, message, sender_email, recipient_list, fail_silently=False)
+            
+            return JsonResponse({'message': 'Mensaje enviado exitosamente.'})
+        
+        except BadHeaderError:
+            return JsonResponse({'message': 'Error en el encabezado del mensaje.'}, status=500)
+        
+        except Exception as e:
+            # Si ocurre cualquier otra excepción, puedes imprimir el mensaje de error para depuración
+            print('Error en el envío del correo:', str(e))
+            return JsonResponse({'message': 'Error al enviar el mensaje.'}, status=500)
 
     else:
-        return render(request, 'contacto.html')  
+        return render(request, 'contacto.html')
