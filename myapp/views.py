e170471e6595ba2cb73b51abc5e0ab4149853237
salgadotomas89 <salgadotomas@outlook.com>
2023-07-26 10:29:36 -0400

    agregando ajax al contactform

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
@@ -9,9 +11,11 @@ from django.contrib import messages
 def home(request):
     return render(request, 'home.html')
 
-
+@csrf_exempt
 def contacto(request):
+    print('hola')
     if request.method == 'POST':
+        print('entre')
         name = request.POST.get('name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
@@ -23,11 +27,8 @@ def contacto(request):
         sender_email = settings.EMAIL_HOST_USER
         recipient_list = ['salgadotomas@icloud.com']  # Coloca aquí la dirección de correo a la que quieres enviar el formulario
         send_mail(subject, message, sender_email, recipient_list, fail_silently=False)
+        print('hello')
+        return JsonResponse({'message': 'Mensaje enviado exitosamente.'})
 
-        # Mostrar mensaje de éxito
-        messages.success(request, 'Mensaje enviado exitosamente.')
-
-        # No redirecciones, sigue mostrando la página de contacto
-        return render(request, 'contacto.html')
     else:
         return render(request, 'contacto.html')  
